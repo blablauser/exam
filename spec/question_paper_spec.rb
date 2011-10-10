2f86d9cea6744521ed0005ea4e616544fa7d82dd
Bla Blauser <khaly_alice@yahoo.com>
2011-10-10 13:06:27 +0100

    step 3,using  empty array makes it go green

--- spec/question_paper_spec.rb
+++ spec/question_paper_spec.rb
@@ -3,7 +3,7 @@ describe QuestionPaper do
   subject {QuestionPaper.new}
 
   it "should have an empty array of questions when QuestionPaper is created" do
-   3333.should be_an_instance_of(Array)
+   [].should be_an_instance_of(Array)
   end
 
 end
